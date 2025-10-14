c4ed808b141fb621339d06ccacad75266b4d371b
valium69mg <xdeus.v2@gmail.com>
2025-10-13 22:10:52 -0600

    feat: add code from init lesson

---
+++ init/init.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func init() {
+	fmt.Println("init function!")
+}
+
+func init() {
+	fmt.Println("init function 2!")
+}
+
+func main() {
+	fmt.Println("main function!")
+}
