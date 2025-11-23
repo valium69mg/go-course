42e9c59057a5164bc09c7e6180b7e61fd7b4d4d3
valium69mg <xdeus.v2@gmail.com>
2025-11-23 17:14:42 -0600

    feat: add code from interfaces lesson

---
+++ interfaces/interfaces.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type geometry interface {
+	area() float64
+	perim() float64
+}
+
+type rectangle struct {
+	width, height float64
+}
+
+type circle struct {
+	radius float64
+}
+
+func (r rectangle) area() float64 {
+	return r.height * r.width
+}
+
+func (c circle) area() float64 {
+	return math.Pi * math.Pow(c.radius, 2)
+}
+
+func (r rectangle) perim() float64 {
+	return 2*r.height + 2*r.width
+}
+
+func (c circle) perim() float64 {
+	return math.Pi * 2 * c.radius
+}
+
+func (c circle) diameter() float64 {
+	return c.radius * 2
+}
+
+func measure(g geometry) {
+	fmt.Println(g)
+	fmt.Println(g.area())
+	fmt.Println(g.perim())
+}
+
+func main() {
+
+	r := rectangle{width: 3, height: 4}
+	c := circle{radius: 5}
+	measure(r)
+	measure(c)
+}
